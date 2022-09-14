f7cc8108664f9b2bebe5549984228d266e3a3d98
Leonid Pospelov <pospelovlm@yandex.ru>
2022-09-14 15:31:15 +0300

    Reflecet vcpkg PR changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const testAddon = require("./build/Debug/test.node");
+const testAddon = (binaryDir) => {
+  const testAddon = require("./" + binaryDir + "/Debug/test.node");
 
-console.log({ function: testAddon.doSomethingUseful });
+  console.log({ function: testAddon.doSomethingUseful });
 
-const result = testAddon.doSomethingUseful();
+  const result = testAddon.doSomethingUseful();
 
-console.log({ result });
+  console.log({ result });
+}
+
+// One of them should fail cause you can't have x86 and x64 nodejs in same time
+testAddon('build');
+testAddon('build32');
